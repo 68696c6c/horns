6823fe484eb14fc1858a75948d1b74837972a6ba
Aaron Hill <68696c6c@gmail.com>
2018-09-16 10:42:17 -0600

    Made the preview area white.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ThemeProvider } from 'emotion-theming'
+import { injectGlobal } from 'emotion'
 import theme from './themes/base'
 
-const App = (props) => {
-  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
-}
+const App = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>
 
 App.propTypes = {
   children: PropTypes.element.isRequired,
 }
 
 export default App
+
+injectGlobal`
+  .react-live > div > div > div > div:first-child {
+    background: white;
+  }
+`
