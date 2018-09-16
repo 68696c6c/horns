fae94eef6bb3d0102891479611f4fcb239708ac8
Aaron Hill <68696c6c@gmail.com>
2018-09-16 12:13:39 -0600

    Refactored the Button component.  Changed Docz menu items.

--- src/components/buttons/button.jsx
+++ src/components/buttons/button.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { cx } from 'react-emotion'
+import styled, { css, cx } from 'react-emotion'
 import { rgb } from '../../themes/utils'
 
+const baseButton = (variant, weight) => {
+  return css`
+    display: inline-block;
+    padding: .6em 2em;
+    font-weight: ${weight};
+    text-align: center;
+    cursor: pointer;
+    background: ${rgb(variant.background)};
+    color: ${rgb(variant.color)};
+    border-radius: ${variant.radius};
+    border: ${variant.border};
+    &:hover {
+      background: ${rgb(variant.hover.background)};
+      color: ${rgb(variant.hover.color)};
+      border: ${variant.hover.border};
+    }
+    &:active {
+      background: ${ rgb(variant.hover.background)};
+      color: ${ rgb(variant.hover.color)};
+      border: ${variant.hover.border};
+    }
+  `
+}
+
 const Styled = styled('button')`
-  display: inline-block;
-  padding: .6em 2em;
-  font-weight: ${props => props.theme.config.fontWeights.bold};
-  text-align: center;
-  cursor: pointer;
-  background: ${props => rgb(props.theme.buttons[props.variant].background)};
-  color: ${props => rgb(props.theme.buttons[props.variant].color)};
-  border-radius: ${props => props.theme.buttons[props.variant].radius};
-  border: ${props => props.theme.buttons[props.variant].border};
-  &:hover {
-    background: ${props => rgb(props.theme.buttons[props.variant].hover.background)};
-    color: ${props => rgb(props.theme.buttons[props.variant].hover.color)};
-    border-radius: ${props => props.theme.buttons[props.variant].hover.radius};
-    border: ${props => props.theme.buttons[props.variant].hover.border};
-  }
-  &:active {
-    background: ${props => rgb(props.theme.buttons[props.variant].hover.background)};
-    color: ${props => rgb(props.theme.buttons[props.variant].hover.color)};
-    border-radius: ${props => props.theme.buttons[props.variant].hover.radius};
-    border: ${props => props.theme.buttons[props.variant].hover.border};
-  }
+  ${props => baseButton(props.theme.buttons[props.variant], props.theme.config.fontWeights.bold)}
 `
 
 const Button = ({ variant, className, children, ...others }) => {
