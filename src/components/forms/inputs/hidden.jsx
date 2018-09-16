5f60bc59add9ace6537b1d8fddd6de426be14fd8
Aaron Hill <68696c6c@gmail.com>
2018-09-16 13:09:42 -0600

    Fixed an error in the InputHidden component.

--- src/components/forms/inputs/hidden.jsx
+++ src/components/forms/inputs/hidden.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { cx } from 'react-emotion'
 
-const InputHidden = ({ id, name, value, required, ...others }) => (
-  <input type="hidden" id={id} name={name} value={value} required={required ? 'required' : ''} {...others} />
+const InputHidden = ({ id, name, value, required, hasError, className, ...others }) => (
+  <input
+    type="hidden"
+    id={id}
+    name={name}
+    value={value}
+    required={required ? 'required' : ''}
+    className={cx(className, hasError ? 'error' : '')}
+    {...others}
+  />
 )
 
 InputHidden.propTypes = {
