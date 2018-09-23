434c6a4d7c621aa325d76bc94370c12d027ac18c
Aaron Hill <68696c6c@gmail.com>
2018-09-23 10:45:14 -0600

    Moved the Section and Grid components to the Layouts directory.

--- src/components/grids/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Area } from './area'
-export { default as Grid } from './grid'
-export { default as GridStacked } from '../grids/stacked'
---
+++ src/components/layout/index.js
@@ -0,0 +1,4 @@
+export { default as Area } from './area'
+export { default as Grid } from './grid'
+export { default as GridStacked } from './stacked'
+export { default as Section } from './section'
