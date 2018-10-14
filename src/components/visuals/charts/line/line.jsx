78d883919d5dc0fde52f7f42adc6176622c53178
Aaron Hill <68696c6c@gmail.com>
2018-10-14 15:20:22 -0600

    Added Header and sticky nav item components.

--- src/components/visuals/charts/line/line.jsx
+++ src/components/visuals/charts/line/line.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import uuid from 'uuid/v4'
-import { SVGCircle } from '../svg'
+import { SVGCircle, SVGPath } from '../svg'
 
 const linePointWidth = 2
 
 const ChartLine = ({ xScale, yInc, yStart, data }) => {
-  const points = []
+  let points = []
+  let path = ''
   for (let i = 0; i < data.length; i++) {
     const y = yStart - (yInc * data[i])
-    points.push(<SVGCircle fill="copy" r={linePointWidth} cx={xScale[i]} cy={y} key={uuid()}/>)
+    const x = xScale[i]
+    if (i === 0) {
+      path += `M ${x} ${y} `
+    } else {
+      path += `L ${x} ${y} `
+    }
+    points.push({ x, y })
   }
-  return <g className="line">{points}</g>
+  return (
+    <g className="line">
+      {points.map(point => <SVGCircle fill="copy" r={linePointWidth} cx={point.x} cy={point.y} key={uuid()}/>)}
+      <SVGPath stroke="copy" d={path} />
+    </g>
+  )
 }
 
 export default ChartLine
