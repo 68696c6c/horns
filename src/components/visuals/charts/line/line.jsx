7842a1d60f5f4a2a4b80a1ee69f40e30330ec999
Aaron Hill <68696c6c@gmail.com>
2018-10-14 16:22:19 -0600

    Improved LineChart styling.

--- src/components/visuals/charts/line/line.jsx
+++ src/components/visuals/charts/line/line.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import uuid from 'uuid/v4'
 import { SVGCircle, SVGPath } from '../svg'
+import { withTheme } from 'emotion-theming'
+import { rgb } from '../../../../themes/utils'
 
 const linePointWidth = 2
 
-const ChartLine = ({ variant, xScale, yInc, yStart, data }) => {
+const ChartLine = ({ theme, variant, xScale, yInc, yStart, data }) => {
   let points = []
   let path = ''
   for (let i = 0; i < data.length; i++) {
@@ -18,10 +20,13 @@ const ChartLine = ({ variant, xScale, yInc, yStart, data }) => {
     }
     points.push({ x, y })
   }
+  const pointColor = theme.colors[variant].dark
+  const lineColor = theme.colors[variant].default
+  const fillColor = theme.colors[variant].alpha
   return (
     <g className="line">
-      {points.map(point => <SVGCircle fill={variant} r={linePointWidth * 2} cx={point.x} cy={point.y} key={uuid()}/>)}
-      <SVGPath stroke={variant} strokeWidth={linePointWidth} d={path} />
+      {points.map(point => <circle fill={rgb(pointColor)} r={linePointWidth * 2} cx={point.x} cy={point.y} key={uuid()}/>)}
+      <path stroke={rgb(lineColor)} fill={rgb(fillColor)} strokeWidth={linePointWidth} d={path} />
     </g>
   )
 }
@@ -52,4 +57,4 @@ ChartLine.defaultProps = {
   variant: 'copy',
 }
 
-export default ChartLine
+export default withTheme(ChartLine)
