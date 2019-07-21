37af250a72c91c21b7a22cb7702c635084f3b9a7
Aaron Hill <68696c6c@gmail.com>
2019-07-21 13:49:27 -0600

    Started working on a proof-of-concept form component.

--- src/components/forms/form.jsx
+++ src/components/forms/form.jsx
@@ -1,78 +1,35 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import { Formik } from 'formik'
-// import * as Yup from 'yup'
-//
-// const fields = {
-//   email: {
-//     value: '',
-//     required: true,
-//   },
-//   password: {
-//     value: '',
-//     required: true,
-//   }
-// }
-//
-// const Form = ({ onSubmit }) => (
-//   <Formik
-//     initialValues={{ email: '', password: '' }}
-//     onSubmit={(values, { setSubmitting }) => {
-//       setTimeout(() => {
-//         onSubmit(JSON.stringify(values, null, 2))
-//         setSubmitting(false)
-//       }, 500)
-//     }}
-//     validationSchema={Yup.object().shape({
-//       email: Yup.string()
-//         .email('Invalid email address')
-//         .required('Please enter your email'),
-//       password: Yup.string().required('Please enter your password'),
-//     })}
-//   >
-//     {({
-//         values,
-//         touched,
-//         errors,
-//         dirty,
-//         isSubmitting,
-//         handleChange,
-//         handleBlur,
-//         handleSubmit,
-//         handleReset,
-//       }) => (
-//       <form onSubmit={handleSubmit}>
-//         <Input
-//           type="email"
-//           name="email"
-//           label="Email"
-//           placeholder="example@xzy.com"
-//           value={values.email}
-//           onChange={handleChange}
-//           onBlur={handleBlur}
-//           required
-//           hasError={errors.email && touched.email}
-//           errorMessage={errors.email}
-//         />
-//         <Input
-//           type="password"
-//           name="password"
-//           label="Password"
-//           placeholder="Password"
-//           value={values.password}
-//           onChange={handleChange}
-//           onBlur={handleBlur}
-//           required
-//           hasError={errors.password && touched.password}
-//           errorMessage={errors.password}
-//         />
-//       </form>
-//     )}
-//   </Formik>
-// )
-//
-// Form.propTypes = {
-//   onSubmit: PropTypes.func.isRequired,
-// }
-//
-// export default Form
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React from 'react'
+
+const fields = [
+  {
+    input: 'text|email|checkbox|select-async|etc...',
+    label: '',
+    placeholder: '',
+    name: '',
+    value: '',
+    required: '',
+    requiredMessage: 'This is required',
+    typeMessage: 'Not a valid email',
+    options: {
+      currency: '$',
+      filterOptions: '',
+    },
+    onClick: '',
+    onChange: '',
+    onKeyUp: '',
+  },
+]
+
+const Form = ({ fields }) => {
+  return (
+    <form />
+  )
+}
+
+Form.propTypes = {}
+
+Form.defaultProps = {}
+
+export default Form
