56f018c63c9c0cc5d314516462dd02b91cac5275
Aaron Hill <68696c6c@gmail.com>
2019-09-28 14:13:16 -0600

    WIP built components for the small map states.

---
+++ src/components/visuals/us-map/states/delaware.jsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React from 'react'
+
+import MapState, {
+  MapStateLabelBackground,
+  MapStateLabel,
+  getMapStateClassName,
+  getMapLabelClassName,
+} from './_map-state'
+
+const abbr = 'DE'
+
+const Delaware = () => (
+  <>
+    <MapState
+      d="M827.78,226.72L824.552,226.684L822.9200000000001,228.28L822.143,229.642L822.611,231.353L826.631,245.19500000000002L829.674,256.315L839.901,254.206L840.329,254.089L840.4839999999999,253.817L839.7429999999999,251.951L838.6529999999999,251.32999999999998L839.1959999999999,249.11299999999997L838.338,246.81899999999996L836.783,247.01499999999996L833.67,244.87899999999996L833.591,243.75099999999998L831.84,242.39099999999996L830.904,238.96899999999997L829.891,236.94899999999996L826.661,234.38499999999996L826.8929999999999,232.71199999999996L825.569,231.23499999999996L827.7819999999999,226.72199999999995Z"
+      transform="matrix(0.741,0,0,0.741,0,0)"
+      className={getMapStateClassName(abbr)}
+    />
+    <MapStateLabelBackground
+      x="631.803"
+      y="210.13901562499998"
+      transform="matrix(0.741,0,0,0.741,169.4645,58.1515)"
+    />
+    <MapStateLabel
+      x="654.303"
+      y="224.523"
+      transform="matrix(0.741,0,0,0.741,169.4645,58.1515)"
+      textY="7.499562499999996"
+      className={getMapLabelClassName(abbr)}
+    >
+      {abbr}
+    </MapStateLabel>
+  </>
+)
+
+export default Delaware
---
+++ src/components/visuals/us-map/states/district-of-columbia.jsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React from 'react'
+
+import MapState, {
+  MapStateLabelBackground,
+  MapStateLabel,
+  getMapStateClassName,
+  getMapLabelClassName,
+} from './_map-state'
+
+const abbr = 'DC'
+
+const DistrictOfColumbia = () => (
+  <>
+    <MapState
+      d="M805.95,253.62L807.62,251.324L804.896,249.77100000000002L803.692,251.25000000000003L804.937,251.59900000000002L805.871,252.49300000000002L805.9499999999999,253.621Z"
+      transform="matrix(0.741,0,0,0.741,0,0)"
+      className={getMapStateClassName(abbr)}
+    />
+    <MapStateLabelBackground
+      x="632.544"
+      y="231.628015625"
+      transform="matrix(0.741,0,0,0.741,169.6564,63.7171)"
+    />
+    <MapStateLabel
+      x="655.044"
+      y="246.012"
+      transform="matrix(0.741,0,0,0.741,169.6564,63.7171)"
+      textY="7.5041875000000005"
+      className={getMapLabelClassName(abbr)}
+    >
+      {abbr}
+    </MapStateLabel>
+  </>
+)
+
+export default DistrictOfColumbia
