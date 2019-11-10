898d373ac2a3c9b36a9684e90909470b9ab6ca1c
james <jamesdbruner@gmail.com>
2019-11-10 14:24:50 -0700

    linted + added proptypes

--- src/components/images/picture.jsx
+++ src/components/images/picture.jsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import styled from '@emotion/styled'
 import { jsx } from '@emotion/core'
-import React from 'react'
 import PropTypes from 'prop-types'
 import { withTheme } from 'emotion-theming'
 
@@ -13,9 +12,20 @@ const Styled = styled('img')`
 // Docz's PropsTable doesn't recognize props defined on higher-order components, so this base component is exported for
 // that purpose.  This component should NOT be exported for use outside this app since it requires the Emotion withTheme
 // HOC in order to function correctly.
-export const PictureBase = ({ alt, srcMin, srcMax, maxWidth, breakpoint, theme, ...others }) => (
+export const PictureBase = ({
+  alt,
+  srcMin,
+  srcMax,
+  maxWidth,
+  breakpoint,
+  theme,
+  ...others
+}) => (
   <picture {...others}>
-    <source media={`(min-width: ${theme.breakpoints[breakpoint]})`} srcSet={srcMax} />
+    <source
+      media={`(min-width: ${theme.breakpoints[breakpoint]})`}
+      srcSet={srcMax}
+    />
     <Styled src={srcMin} alt={alt} maxWidth={maxWidth} />
   </picture>
 )
@@ -29,6 +39,8 @@ PictureBase.propTypes = {
 }
 
 PictureBase.defaultProps = {
+  alt: '',
+  srcMax: '',
   maxWidth: 'initial',
   breakpoint: 'medium',
 }
