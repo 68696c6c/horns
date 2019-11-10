9be3609728a092276de7af1d4b9b979e36a5590c
james <jamesdbruner@gmail.com>
2019-11-10 13:09:21 -0700

    removed useless fragment

--- src/components/layout/section.stories.jsx
+++ src/components/layout/section.stories.jsx
@@ -1,35 +1,27 @@
 import React from 'react'
 
-// Import the theme
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs'
 import Section from './section'
 
 export default { title: 'Section', decorators: [withKnobs] }
 
 export const example = () => (
-  <>
-    <Section
-      colorway={select('colorway', {
-        warning: 'warning',
-        danger: 'danger',
-        info: 'info',
-      })}
-      contained={boolean('contained', false)}
-      padded={boolean('padded', true)}
-    >
-      <p>
-        {text(
-          'content',
-          'These Sections have had their padding highlighted to illustrate how the padding works.'
-        )}
-      </p>
-    </Section>
-  </>
+  <Section
+    colorway={select('colorway', {
+      default: '',
+      warning: 'warning',
+      danger: 'danger',
+      info: 'info',
+    })}
+    contained={boolean('contained', false)}
+    padded={boolean('padded', true)}
+  >
+    <p>
+      {text(
+        'content',
+        `Loki O-Yama Shamad Beelzebub elanu saha caelazod: Do-o-i-ape mada: goholore, gohus, amiranu! Micama! Yehusozod ca-ca-com, od do-o-a-inu noari micaolazoda a-ai-om. Dagon Sedit Dracula Casarameji gohia: Zodacare! Vaunigilaji! od im-ua-mar pugo pelapeli Ananael Qo-a-an. Beelzebub Marduk Do-o-i-ape mada: goholore, gohus, amiranu! Micama! Hecate Ili e-Ol balazodareji, od aala tahilanu-os netaabe: daluga vaomesareji elonusa cape-mi-ali varoesa cala homila; Astaroth Mammon Ishtar Shiva Damballa Mania Sekhmet Ahriman Mormo`
+      )}
+    </p>
+  </Section>
 )
-
-// Knobs for React props
-// export const withAButton = () => (
-//   <button disabled={boolean('Disabled', false)}>
-//     {text('Label', 'Hello Storybook')}
-//   </button>
-// )
