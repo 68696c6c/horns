5cfcf2fb831c5099f3333730b91cabaf4673b333
james <jamesdbruner@gmail.com>
2019-11-10 14:22:00 -0700

    eslint

--- src/components/_demo/wrappers/wrapper.jsx
+++ src/components/_demo/wrappers/wrapper.jsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import styled from '@emotion/styled'
 import { jsx } from '@emotion/core'
-import React from 'react'
 import PropTypes from 'prop-types'
 import { wrapperStyle } from './base'
 
@@ -9,14 +8,18 @@ const Styled = styled('div')`
   ${({ variant, theme }) => wrapperStyle(theme.colors[variant].default, theme)};
 `
 
-const Wrapper = ({ variant, children }) => <Styled variant={variant}>{children}</Styled>
+const Wrapper = ({ variant, children }) => (
+  <Styled variant={variant}>{children}</Styled>
+)
 
 Wrapper.propTypes = {
   variant: PropTypes.string,
+  children: PropTypes.node,
 }
 
 Wrapper.defaultProps = {
   variant: 'background',
+  children: null,
 }
 
 export default Wrapper
