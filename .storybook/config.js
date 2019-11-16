393d438e90b796ac3109be88b890d37b31794fd6
james <jamesdbruner@gmail.com>
2019-11-16 14:33:15 -0700

    removed margin/padding in storybook iframe

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,6 +7,10 @@ import theme from '../src/themes/base'
 // Checks in our src folder for any files with `.stories.jsx` file extension and adds them to storybook
 configure(require.context('../src', true, /\.stories\.jsx$/), module)
 
+// Remove the padding/margin from storybook
+const styles = `html, body {margin: 0; padding: 0;}`
+
 // Wraps all of our stories in our ThemeProvider with a theme
-const themeProviderDecorator = storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+const themeProviderDecorator = storyFn => <ThemeProvider theme={theme}><style type="text/css">{styles}</style>{storyFn()}</ThemeProvider>
+
 addDecorator(themeProviderDecorator)
